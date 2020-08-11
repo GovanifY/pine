d7bc54645ef2ba3a81514eaaa0675c092838997b
Gauvain 'GovanifY' Roussel-Tarbouriech <gauvain@govanify.com>
2020-08-11 08:39:39 +0200

    cleaner and documented code

--- client.cpp
+++ client.cpp
@@ -7,54 +7,46 @@
 #include <thread>
 #include <unistd.h>
 
-#ifdef __linux__
-
+#ifdef _WIN32
+#include <windows.h>
+#else
 #include <netdb.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #endif
-#ifdef _WIN32
-#include <windows.h>
-#define PORT 28011
-#endif
 
-#define to64b(arr) (((uint64_t)(((uint8_t *)(arr))[7]) <<  0)+\
-        ((uint64_t)(((uint8_t *)(arr))[6]) <<  8)+\
-        ((uint64_t)(((uint8_t *)(arr))[5]) << 16)+\
-        ((uint64_t)(((uint8_t *)(arr))[4]) << 24)+\
-        ((uint64_t)(((uint8_t *)(arr))[3]) << 32)+\
-        ((uint64_t)(((uint8_t *)(arr))[2]) << 40)+\
-        ((uint64_t)(((uint8_t *)(arr))[1]) << 48)+\
-        ((uint64_t)(((uint8_t *)(arr))[0]) << 56))
+/* Quick HOWTO:
+ * The Entire PCSX2IPC API is below, you can find it clearly defined between the
+ * command just below me with a bunch of equals and a relatively similar one at
+ * the end of the class, just search for equals.
+ *
+ * This API exposes a result value which is assumed to be checked after
+ * executing a command. If you want to see a list of currently implemented API
+ * functions currently usable by the end user of this API look up the "public:"
+ * keyword of this class and read the comments of the functions.
+ *
+ * A small usage example is available at the end of this file.
+ *
+ * /!\ ON WINDOWS YOU HAVE TO INITIALIZE SOCKETS ONCE OUT OF THIS API AND CLEAN
+ * THEM UP AFTERWARDS, THIS CANNOT BE DONE FROM THE API, SEE THE USAGE EXAMPLE /!\
+ *
+ * Have fun!
+ * -GovanifY, 2020 */
 
-#define to32b(arr) (((uint32_t)(((uint8_t *)(arr))[3]) <<  0)+\
-        ((uint32_t)(((uint8_t *)(arr))[2]) <<  8)+\
-        ((uint32_t)(((uint8_t *)(arr))[1]) << 16)+\
-        ((uint32_t)(((uint8_t *)(arr))[0]) << 24))
+/* ========================= PCSX2IPC API =====================*/
 
-#define to16b(arr) (((uint16_t)(((uint8_t *)(arr))[1]) <<  0)+\
-        ((uint16_t)(((uint8_t *)(arr))[0]) <<  8))
-
-#define to8b(arr) (((uint8_t)(((uint8_t *)(arr))[0]) <<  0))
-
-
-
-void msleep(int sleepMs)
-{
-#ifdef __linux__
-    usleep(sleepMs * 1000);   // usleep takes sleep time in us (1 millionth of a second)
-#endif
-#ifdef _WIN32
-    Sleep(sleepMs);
-#endif
-}
 
 
 class PCSX2Ipc {
-    public:
+    protected:
+#ifdef _WIN32
+        #define PORT 28011
+#else
         const char* SOCKET_NAME = "/tmp/pcsx2.sock";
+#endif
 
+        // possible command messages
         enum IPCCommand {
             MsgRead8 = 0,
             MsgRead16 = 1,
@@ -66,14 +58,45 @@ class PCSX2Ipc {
             MsgWrite64 = 7
         };
 
-        enum Status {
-            Fail,
-            Success,
-            Unknown
+        
+        // possible result codes
+        enum IPCResult {
+            IPC_OK = 0,
+            IPC_FAIL = 0xFF
         };
-        Status result = Unknown;
 
-        int SendCommand(std::tuple<int, char*> command, std::tuple<int, char*> ret) {
+        /* Converts an uint to an char* in little endian 
+         * res_array: the array to modify 
+         * res: the value to convert
+         * i: when to insert it into the array 
+         * return value: res_array */
+        template <typename T>
+        static char* ToArray(char* res_array, T res, int i) {
+           for(int y=sizeof(T); y > 0; y--) {
+               res_array[i-(y-sizeof(T))] = (unsigned char)(res >> ((y-1)*8)) & 0xff;
+           }
+           return res_array;
+        }
+
+        /* Converts a char* to an uint in little endian 
+         * arr: the array to convert
+         * i: when to load it from the array 
+         * return value: the converted value */
+        template <typename T>
+        static T FromArray(char* arr, int i) {
+            T res = 0;
+            for(int y=sizeof(T); y > 0; y--) {
+                res += (((T)(((uint8_t*)(arr))[i-(y-sizeof(T))]) << ((y-1)*8)));
+            }
+            return res;
+        }
+
+
+        /* Internal function, sends an IPC command.
+         * command: pair containing the IPC command size and buffer.
+         * ret: pair containing the IPC return size and buffer.
+         * return value: 0 on success, -1 on failure */
+        int SendCommand(std::pair<int, char*> command, std::pair<int, char*> ret) {
 #ifdef _WIN32
             SOCKET sock;
             struct sockaddr_in server;
@@ -114,142 +137,177 @@ class PCSX2Ipc {
 #endif
 
 #ifdef _WIN32
-            if (send(sock, std::get<1>(command), std::get<0>(command),0) < 0) {
+            if (send(sock, command.second, command.first,0) < 0) {
 #else
-            if (write(sock, std::get<1>(command), std::get<0>(command)) < 0) {
+            if (write(sock, command.second, command.first) < 0) {
 #endif
                 return -1;
             }
 
 #ifdef _WIN32
-            if (recv(sock, std::get<1>(ret), std::get<0>(ret), 0) < 0) {
+            if (recv(sock, ret.second, ret.first, 0) < 0) {
 #else
-            if (read(sock, std::get<1>(ret), std::get<0>(ret)) < 0) {
+            if (read(sock, ret.second, ret.first) < 0) {
 #endif
                 return -1;
             }
             close(sock);
 
-            if(std::get<1>(ret)[0] == (char)0xFF) {
+            if(ret.second[0] == (char)IPC_FAIL) {
                 return -1;
             }
             return 0;
         }
-        char* FormatBeginning(char* cmd, uint32_t address, IPCCommand command) {
+
+
+
+        /* Formats an IPC buffer
+         * size: size of the array to allocate
+         * address: address to put as arg0 of the IPC command
+         * command: IPC message tag
+         * return value: buffer containing the status code and arg0 allocated of size
+         *               size */
+        char* FormatBeginning(int size, uint32_t address, IPCCommand command) {
+            char* cmd = (char*)malloc(size*sizeof(char));
             cmd[0] = (unsigned char) command;
-            cmd[1] = (unsigned char) (address >> 24) & 0xff;
-            cmd[2] = (unsigned char) (address >> 16) & 0xff;
-            cmd[3] = (unsigned char) (address >> 8) & 0xff;
-            cmd[4] = (unsigned char) address;
-            return cmd;
+            return ToArray(cmd, address, 1);
         }
 
+        /* Allocated an array
+         * size: size of the array to allocate
+         * return value: array */
+        char* ResArray(int size) {
+            char* cmd = (char*)malloc(size*sizeof(char));
+            return  cmd;
+        }
+
+
+    public:
+
+        // status of the last operation of this object
+        enum Status {
+            Fail,
+            Success,
+            Unknown
+        };
+        Status result = Unknown;
+
+        /* Reads an 8bit value from PCSX2 game memory 
+         * address: the address to read
+         * return value: the value */
         uint8_t Read8(uint32_t address) {
-            char* cmd = (char*)malloc(5*sizeof(char));
-            FormatBeginning(cmd, address, MsgRead8);
-            char* res_array = (char*)malloc(2*sizeof(char));
-            if (SendCommand(std::make_tuple(5,cmd), std::make_tuple(2,res_array)) < 0) {
+            char* res = ResArray(2);
+            if (SendCommand(std::make_pair(5,FormatBeginning(5, address, MsgRead8)), std::make_pair(2,res)) < 0) {
                 result = Fail;
             } else { result = Success; }
-            return to8b(&res_array[1]);
+            return FromArray<uint8_t>(res, 1);
         }
 
+        /* Reads a 16bit value from PCSX2 game memory 
+         * address: the address to read
+         * return value: the value */
         uint16_t Read16(uint32_t address) {
-            char* cmd = (char*)malloc(5*sizeof(char));
-            FormatBeginning(cmd, address, MsgRead16);
-            char* res_array = (char*)malloc(3*sizeof(char));
-            if (SendCommand(std::make_tuple(5,cmd), std::make_tuple(3,res_array)) < 0) {
+            char* res = ResArray(3);
+            if (SendCommand(std::make_pair(5,FormatBeginning(5, address, MsgRead16)), std::make_pair(3,res)) < 0) {
                 result = Fail;
             } else { result = Success; }
-            return to16b(&res_array[1]);
+            return FromArray<uint16_t>(res, 1);
         }
 
+        /* Reads a 32bit value from PCSX2 game memory 
+         * address: the address to read
+         * return value: the value */
         uint32_t Read32(uint32_t address) {
-            char* cmd = (char*)malloc(5*sizeof(char));
-            FormatBeginning(cmd, address, MsgRead32);
-            char* res_array = (char*)malloc(5*sizeof(char));
-            if (SendCommand(std::make_tuple(5,cmd), std::make_tuple(5,res_array)) < 0) {
+            char* res = ResArray(5);
+            if (SendCommand(std::make_pair(5,FormatBeginning(5, address, MsgRead32)), std::make_pair(5,res)) < 0) {
                 result = Fail;
             } else { result = Success; }
-            return to32b(&res_array[1]);
+            return FromArray<uint32_t>(res, 1);
         }
 
+        /* Reads a 64bit value from PCSX2 game memory 
+         * address: the address to read
+         * return value: the value */
         uint64_t Read64(uint32_t address) {
-            char* cmd = (char*)malloc(5*sizeof(char));
-            FormatBeginning(cmd, address, MsgRead64);
-            char* res_array = (char*)malloc(9*sizeof(char));
-            if (SendCommand(std::make_tuple(5,cmd), std::make_tuple(9,res_array)) < 0) {
+            char* res = ResArray(9);
+            if (SendCommand(std::make_pair(5,FormatBeginning(5, address, MsgRead64)), std::make_pair(9,res)) < 0) {
                 result = Fail;
             } else { result = Success; }
-            return to64b(&res_array[1]);
+            return FromArray<uint64_t>(res, 1);
         }
 
+        /* Writes an 8bit value to PCSX2 game memory 
+         * address: the address to write to
+         * value: the value */
         void Write8(uint32_t address, uint8_t value) {
-            char* cmd = (char*)malloc(6*sizeof(char));
-            FormatBeginning(cmd, address, MsgWrite8);
-            char* res_array = (char*)malloc(1*sizeof(char));
-            cmd[5] = (char)value;
-            if (SendCommand(std::make_tuple(6,cmd), std::make_tuple(1,res_array)) < 0) {
+            FormatBeginning(6, address, MsgWrite8);
+            char* cmd = ToArray(FormatBeginning(6, address, MsgWrite8), value, 5);
+            if (SendCommand(std::make_pair(6,cmd), std::make_pair(1,ResArray(1))) < 0) {
                 result = Fail;
             } else { result = Success; }
         }
 
+        /* Writes a 16bit value to PCSX2 game memory 
+         * address: the address to write to
+         * value: the value */
         void Write16(uint32_t address, uint16_t value) {
-            char* cmd = (char*)malloc(7*sizeof(char));
-            FormatBeginning(cmd, address, MsgWrite16);
-            char* res_array = (char*)malloc(1*sizeof(char));
-            cmd[5] = (unsigned char) (value >> 8) & 0xff;
-            cmd[6] = (unsigned char) value;
-            if (SendCommand(std::make_tuple(7,cmd), std::make_tuple(1,res_array)) < 0) {
+            char* cmd = ToArray(FormatBeginning(7, address, MsgWrite16), value, 5);
+            if (SendCommand(std::make_pair(7,cmd), std::make_pair(1,ResArray(1))) < 0) {
                 result = Fail;
             } else { result = Success; }
         }
 
+        /* Writes a 32bit value to PCSX2 game memory 
+         * address: the address to write to
+         * value: the value */
         void Write32(uint32_t address, uint32_t value) {
-            char* cmd = (char*)malloc(9*sizeof(char));
-            FormatBeginning(cmd, address, MsgWrite32);
-            char* res_array = (char*)malloc(1*sizeof(char));
-            cmd[5] = (unsigned char) (value >> 24) & 0xff;
-            cmd[6] = (unsigned char) (value >> 16) & 0xff;
-            cmd[7] = (unsigned char) (value >> 8) & 0xff;
-            cmd[8] = (unsigned char) value;
-            if (SendCommand(std::make_tuple(9,cmd), std::make_tuple(1,res_array)) < 0) {
+            char* cmd = ToArray(FormatBeginning(9, address, MsgWrite32), value, 5);
+            if (SendCommand(std::make_pair(9,cmd), std::make_pair(1,ResArray(1))) < 0) {
                 result = Fail;
             } else { result = Success; }
         }
 
+        /* Writes a 64bit value to PCSX2 game memory 
+         * address: the address to write to
+         * value: the value */
         void Write64(uint32_t address, uint64_t value) {
-            char* cmd = (char*)malloc(13*sizeof(char));
-            FormatBeginning(cmd, address, MsgWrite64);
-            char* res_array = (char*)malloc(1*sizeof(char));
-            cmd[5] = (unsigned char) (value >> 56) & 0xff;
-            cmd[6] = (unsigned char) (value >> 48) & 0xff;
-            cmd[7] = (unsigned char) (value >> 40) & 0xff;
-            cmd[8] = (unsigned char) (value >> 32) & 0xff;
-            cmd[9] = (unsigned char) (value >> 24) & 0xff;
-            cmd[10] = (unsigned char) (value >> 16) & 0xff;
-            cmd[11] = (unsigned char) (value >> 8) & 0xff;
-            cmd[12] = (unsigned char) value;
-            if (SendCommand(std::make_tuple(13,cmd), std::make_tuple(1,res_array)) < 0) {
+            char* cmd = ToArray(FormatBeginning(13, address, MsgWrite64), value, 5);
+            if (SendCommand(std::make_pair(13,cmd), std::make_pair(1,ResArray(1))) < 0) {
                 result = Fail;
             } else { result = Success; }
         }
 
 };
 
+/* ========================= PCSX2IPC API END =====================*/
+
+
+
+// a portable sleep function
+void msleep(int sleepMs) {
+#ifdef _WIN32
+    Sleep(sleepMs);
+#else
+    usleep(sleepMs * 1000);   // usleep takes sleep time in us (1 millionth of a second)
+#endif
+}
+
+
 // this function is an infinite loop reading a value in memory, this shows you
 // how timer can work
 void read_background() {
+
+   // we instantiate a new PCSX2Ipc object, it is a good idea to create a new
+   // one per thread, so you have a result code for each operation to verify
+   // on.
+   // You are NOT obligated to verify error codes but it is good practice to do
+   // so.
+   PCSX2Ipc* test = new PCSX2Ipc();
+
     while(true) {
         // you can go slower but go higher at your own risk
         msleep(100);
 
-        // we instantiate a new PCSX2Ipc object, it is a good idea to create a new
-        // one per thread, so you have a result code for each operation to verify
-        // on.
-        // You are NOT obligated to verify error codes but it is good practice to do
-        // so.
-        PCSX2Ipc* test = new PCSX2Ipc();
 
         // we read a 32 bit value from memory address 0x00347D34
         uint32_t value = test->Read32(0x00347D34);
@@ -266,8 +324,10 @@ void read_background() {
 
 }
 
+// the main function that is executed at the start of our program
 int main(int argc, char *argv[]) {
 
+    // we initialize winsock
 #ifdef _WIN32
     WSADATA wsa;
     WSAStartup(MAKEWORD(2, 2), &wsa);
@@ -280,12 +340,22 @@ int main(int argc, char *argv[]) {
     msleep(5000);
     PCSX2Ipc* test = new PCSX2Ipc();
     test->Write8(0x00347D34, 0xFF);
-    printf("PCSX2Ipc::Write8(0x00347D34, 255)\n");
+    // if the operation failed
+    if (test->result != test->Success) {
+        printf("ERROR!!!!!\n");
+    }
+    else {
+        // otherwise we print the result
+        printf("PCSX2Ipc::Write8(0x00347D34, 255)\n");
+    }
+
 
     // we wait for the thread to finish. in our case it is an infinite loop
     // (while true) so it will never do so.
     first.join();
 
+
+    // we clean up winsock
 #ifdef _WIN32
     WSACleanup();
 #endif
