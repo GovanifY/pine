e4d2b38784a011841a0ff686cbb767602267d810
chaoticgd <43898262+chaoticgd@users.noreply.github.com>
2026-07-14 21:58:35 +0100

    Fix the tests
    - Remove an unsafe strcpy call.
    - Update the PCSX2 executable name for the age of Qt.
    - Don't leak the PINE::PCSX2 objects.
    - Don't send requests containing an uninitialized message size field.
    - Fix a deadlock where the "packets are too big" tests wouldn't unlock
      the batch_blocking and ipc_blocking mutexes.

--- src/tests.cpp
+++ src/tests.cpp
@@ -27,8 +27,7 @@ auto msleep(int sleepMs) -> void {
 auto open_pcsx2() -> void {
     if (const char *env_p = std::getenv("PCSX2_TEST")) {
         char pcsx2_path[4096];
-        strcpy(pcsx2_path, env_p);
-        strcat(pcsx2_path, " &");
+        snprintf(pcsx2_path, sizeof(pcsx2_path), "%s &", env_p);
         if (system(pcsx2_path) != 0) {
             printf("PCSX2 failed to execute! Have you set your environment "
                    "variables?\n");
@@ -37,9 +36,9 @@ auto open_pcsx2() -> void {
 }
 
 #ifdef _WIN32
-auto kill_pcsx2() -> int { return system("tskill PCSX2"); }
+auto kill_pcsx2() -> int { return system("tskill pcsx2-qt"); }
 #else
-auto kill_pcsx2() -> int { return system("pkill PCSX2"); }
+auto kill_pcsx2() -> int { return system("pkill pcsx2-qt"); }
 
 #endif
 
@@ -50,8 +49,8 @@ SCENARIO("PCSX2 can be interacted with remotely through IPC", "[pine]") {
 
     WHEN("PCSX2 is not started") {
         THEN("Errors should happen") {
-            PINE::PCSX2 *ipc = new PINE::PCSX2();
-            REQUIRE_THROWS(ipc->Write<u64>(0x00347D34, 5));
+            PINE::PCSX2 ipc;
+            REQUIRE_THROWS(ipc.Write<u64>(0x00347D34, 5));
         }
     }
 
@@ -60,59 +59,61 @@ SCENARIO("PCSX2 can be interacted with remotely through IPC", "[pine]") {
         open_pcsx2();
 
         // we wait for PCSX2 to be reachable within 10 seconds.
-        PINE::PCSX2 *check = new PINE::PCSX2();
-        int i = 0;
-        while (i < 20) {
-            try {
-                check->Read<u8>(0x00347D34);
-                break;
-            } catch (...) {
-                msleep(500);
-                i++;
-                // F
-                if (i >= 20)
-                    REQUIRE(0 == 1);
+        {
+            PINE::PCSX2 check;
+            int i = 0;
+            while (i < 20) {
+                try {
+                    check.Read<u8>(0x00347D34);
+                    break;
+                } catch (...) {
+                    msleep(500);
+                    i++;
+                    // F
+                    if (i >= 20)
+                        REQUIRE(0 == 1);
+                }
             }
         }
 
         WHEN("We want to communicate with PCSX2") {
             THEN("It returns errors on invalid commands") {
-                PINE::PCSX2 *ipc = new PINE::PCSX2();
+                PINE::PCSX2 ipc;
 
-                char c_cmd[5];
+                char c_cmd[5] = {};
                 c_cmd[4] = PINE::PCSX2::MsgUnimplemented;
                 c_cmd[0] = 5;
-                char c_ret[5];
+                char c_ret[5] = {};
 
                 // send unimplement message to server
                 REQUIRE_THROWS(
-                    ipc->SendCommand(PINE::PCSX2::IPCBuffer{ 5, c_cmd },
-                                     PINE::PCSX2::IPCBuffer{ 5, c_ret }));
+                    ipc.SendCommand(PINE::PCSX2::IPCBuffer{ 5, c_cmd },
+                                    PINE::PCSX2::IPCBuffer{ 5, c_ret }));
 
                 // make unimplemented write
-                REQUIRE_THROWS(ipc->Write<u128>(0x00347D34, 5));
+                REQUIRE_THROWS(ipc.Write<u128>(0x00347D34, 5));
 
                 // make unimplemented read
-                REQUIRE_THROWS(ipc->Read<u128>(0x00347D34));
+                REQUIRE_THROWS(ipc.Read<u128>(0x00347D34));
             }
 
             THEN("It returns errors when socket issues happen") {
-                PINE::PCSX2 *ipc = new PINE::PCSX2();
+                PINE::PCSX2 ipc;
 
-                char c_cmd[5];
+                char c_cmd[5] = {};
                 c_cmd[0] = 5;
 
                 // trying to read from socket with INT_MAX and/or pointer from
                 // an address space that is not yours will return an errno. We
                 // do both to simulate a connection issue.
-                REQUIRE_THROWS(ipc->SendCommand(
+                REQUIRE_THROWS(ipc.SendCommand(
                     PINE::PCSX2::IPCBuffer{ 5, c_cmd },
                     PINE::PCSX2::IPCBuffer{ INT_MAX, (char *)0x00 }));
 
                 // trying to write to a socket with INT_MAX and/or pointer from
                 // an address space that is not yours will return an errno. We
                 // do both to simulate a connection issue.
-                REQUIRE_THROWS(ipc->SendCommand(
+                REQUIRE_THROWS(ipc.SendCommand(
                     PINE::PCSX2::IPCBuffer{ INT_MAX, (char *)0x00 },
                     PINE::PCSX2::IPCBuffer{ 1, c_cmd }));
             }
@@ -124,15 +125,15 @@ SCENARIO("PCSX2 can be interacted with remotely through IPC", "[pine]") {
                 // we do a bunch of writes, read back, ensure the result is
                 // correct and ensure we didn't threw an error.
                 REQUIRE_NOTHROW([&]() {
-                    PINE::PCSX2 *ipc = new PINE::PCSX2();
-                    ipc->Write<u64>(0x00347D34, 5);
-                    ipc->Write<u32>(0x00347D44, 6);
-                    ipc->Write<u16>(0x00347D54, 7);
-                    ipc->Write<u8>(0x00347D64, 8);
-                    REQUIRE(ipc->Read<u64>(0x00347D34) == 5);
-                    REQUIRE(ipc->Read<u32>(0x00347D44) == 6);
-                    REQUIRE(ipc->Read<u16>(0x00347D54) == 7);
-                    REQUIRE(ipc->Read<u8>(0x00347D64) == 8);
+                    PINE::PCSX2 ipc;
+                    ipc.Write<u64>(0x00347D34, 5);
+                    ipc.Write<u32>(0x00347D44, 6);
+                    ipc.Write<u16>(0x00347D54, 7);
+                    ipc.Write<u8>(0x00347D64, 8);
+                    REQUIRE(ipc.Read<u64>(0x00347D34) == 5);
+                    REQUIRE(ipc.Read<u32>(0x00347D44) == 6);
+                    REQUIRE(ipc.Read<u16>(0x00347D54) == 7);
+                    REQUIRE(ipc.Read<u8>(0x00347D64) == 8);
                 }());
             }
         }
@@ -143,8 +144,8 @@ SCENARIO("PCSX2 can be interacted with remotely through IPC", "[pine]") {
                 // we do a bunch of writes, read back, ensure the result is
                 // correct and ensure we didn't threw an error.
                 REQUIRE_NOTHROW([&]() {
-                    PINE::PCSX2 *ipc = new PINE::PCSX2();
-                    char *version = ipc->Version();
+                    PINE::PCSX2 ipc;
+                    char *version = ipc.Version();
                     REQUIRE(strncmp(version, "PCSX2", 5) == 0);
                 }());
             }
@@ -157,58 +158,55 @@ SCENARIO("PCSX2 can be interacted with remotely through IPC", "[pine]") {
                 // correct and ensure we didn't threw an error, in batch mode
                 // this time.
                 REQUIRE_NOTHROW([&]() {
-                    PINE::PCSX2 *ipc = new PINE::PCSX2();
+                    PINE::PCSX2 ipc;
 
-                    ipc->InitializeBatch();
-                    ipc->Write<u64, true>(0x00347E34, 5);
-                    ipc->Write<u32, true>(0x00347E44, 6);
-                    ipc->Write<u16, true>(0x00347E54, 7);
-                    ipc->Write<u8, true>(0x00347E64, 8);
-                    ipc->SendCommand(ipc->FinalizeBatch());
+                    ipc.InitializeBatch();
+                    ipc.Write<u64, true>(0x00347E34, 5);
+                    ipc.Write<u32, true>(0x00347E44, 6);
+                    ipc.Write<u16, true>(0x00347E54, 7);
+                    ipc.Write<u8, true>(0x00347E64, 8);
+                    ipc.SendCommand(ipc.FinalizeBatch());
 
                     msleep(1);
 
-                    ipc->InitializeBatch();
-                    ipc->Read<u64, true>(0x00347E34);
-                    ipc->Read<u32, true>(0x00347E44);
-                    ipc->Read<u16, true>(0x00347E54);
-                    ipc->Read<u8, true>(0x00347E64);
-                    ipc->Version<true>();
-                    auto resr = ipc->FinalizeBatch();
-                    ipc->SendCommand(resr);
+                    ipc.InitializeBatch();
+                    ipc.Read<u64, true>(0x00347E34);
+                    ipc.Read<u32, true>(0x00347E44);
+                    ipc.Read<u16, true>(0x00347E54);
+                    ipc.Read<u8, true>(0x00347E64);
+                    ipc.Version<true>();
+                    auto resr = ipc.FinalizeBatch();
+                    ipc.SendCommand(resr);
 
                     REQUIRE(
-                        strncmp(ipc->GetReply<PINE::PCSX2::MsgVersion>(resr, 4),
+                        strncmp(ipc.GetReply<PINE::PCSX2::MsgVersion>(resr, 4),
                                 "PCSX2", 5) == 0);
-                    REQUIRE(ipc->GetReply<PINE::PCSX2::MsgRead8>(resr, 3) == 8);
-                    REQUIRE(ipc->GetReply<PINE::PCSX2::MsgRead16>(resr, 2) ==
-                            7);
-                    REQUIRE(ipc->GetReply<PINE::PCSX2::MsgRead32>(resr, 1) ==
-                            6);
-                    REQUIRE(ipc->GetReply<PINE::PCSX2::MsgRead64>(resr, 0) ==
-                            5);
+                    REQUIRE(ipc.GetReply<PINE::PCSX2::MsgRead8>(resr, 3) == 8);
+                    REQUIRE(ipc.GetReply<PINE::PCSX2::MsgRead16>(resr, 2) == 7);
+                    REQUIRE(ipc.GetReply<PINE::PCSX2::MsgRead32>(resr, 1) == 6);
+                    REQUIRE(ipc.GetReply<PINE::PCSX2::MsgRead64>(resr, 0) == 5);
                 }());
             }
 
             THEN("We error out when packets are too big") {
-                PINE::PCSX2 *ipc = new PINE::PCSX2();
-
                 // write packets too big
                 REQUIRE_THROWS([&]() {
-                    ipc->InitializeBatch();
+                    PINE::PCSX2 ipc;
+                    ipc.InitializeBatch();
                     for (int i = 0; i < 60000; i++) {
-                        ipc->Write<u64, true>(0x00347E34, 5);
+                        ipc.Write<u64, true>(0x00347E34, 5);
                     }
-                    ipc->SendCommand(ipc->FinalizeBatch());
+                    ipc.SendCommand(ipc.FinalizeBatch());
                 }());
 
                 // read packets too big
                 REQUIRE_THROWS([&]() {
-                    ipc->InitializeBatch();
+                    PINE::PCSX2 ipc;
+                    ipc.InitializeBatch();
                     for (int i = 0; i < 60000; i++) {
-                        ipc->Read<u64, true>(0x00347E34);
+                        ipc.Read<u64, true>(0x00347E34);
                     }
-                    ipc->SendCommand(ipc->FinalizeBatch());
+                    ipc.SendCommand(ipc.FinalizeBatch());
                 }());
             }
         }
